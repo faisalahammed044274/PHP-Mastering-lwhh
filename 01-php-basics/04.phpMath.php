d9478f5e3314059679f0715c5a24c88d26a68a77
faisalahammed044274 <faisalahammed044274@gmail.com>
2024-08-21 02:24:22 +0600

    modulas obosisto

--- 01-php-basics/04.phpMath.php
+++ 01-php-basics/04.phpMath.php
@@ -8,8 +8,9 @@ $plus = $number + 20;
 $minus = $number - 2;
 $multiply = $number * 2;
 $divide = $number / 3;
+$modulas = 5 % 2;
 
 
-echo $plus,"\n", $minus,"\n", $multiply,"\n", $divide;
+echo $plus,"\n", $minus,"\n", $multiply,"\n", $divide,"\n", $modulas;
 
 //BODMAS = Bracket Or Division Multiplication Addition Substraction
