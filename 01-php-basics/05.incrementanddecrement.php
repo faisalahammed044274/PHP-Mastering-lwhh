1ffd25c0741afc89e158cc6b67543837e5c69067
faisalahammed044274 <faisalahammed044274@gmail.com>
2024-08-25 20:42:15 +0600

    numbers- hex octa binary

---
+++ 01-php-basics/05.incrementanddecrement.php
@@ -0,0 +1,28 @@
+<?php
+$a =3 + 2 ; //operend operator
+echo $a;
+
+echo PHP_EOL;
+
+$x = $y = $z =2; //multiple variable assign
+
+echo $x . " <---> " .$y . " <---> " .$z;
+
+$m = 7;
+
+$n = $m++;
+echo PHP_EOL;
+echo $m++, "\n" ,++$n;
+
+
+/*
+//$n = $m++;
+$n = $m;
+$n = $m + 1;
+*/
+
+/*
+//$n = ++$m;
+$n = $m +1;
+$n = $m;
+*/
---
+++ 01-php-basics/06.number-system.php
@@ -0,0 +1,14 @@
+<?php
+
+
+$a =12; // Binary
+
+$b = 012; //octal number 8 nai octal e
+
+$c = 0x2B; // Decimal
+
+printf("The number is %d and %d another is %d", $a, $b,$c);
+
+printf("\nThe binary equivalent of %d is %b",143,143);
+printf("\nThe Hexadecimal equivalent of %d is %x",143,143);
+printf("\nThe Octal equivalent of %d is %o",143,143);
