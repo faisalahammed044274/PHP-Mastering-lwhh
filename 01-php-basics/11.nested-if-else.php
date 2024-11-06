5e9fe2c52f48550f950646b9ff073a268ef8f7bb
faisalahammed044274 <faisalahammed044274@gmail.com>
2024-11-06 12:52:13 +0600

    Nested If Else

--- 01-php-basics/11.nested-if-else.php
+++ 01-php-basics/11.nested-if-else.php
@@ -1,31 +1,41 @@
 <?php
 
-if(true){
-    echo " It's true 1 \n";
-    if(false){ 
-        echo "it's true 2 \n";
-    } else {
-        echo "Hello False 2 \n";
+$condition1 = 1+1 == 2;
+$condition2 = 2+2 == 4;
+$condition3 = 3+3 == 6;
+$condition4 = 4+1 == 5;
+
+if($condition1){
+echo "Okay! Condition 1 \n";
+    if($condition2){
+        echo "Okay! Condition 2 \n";
+        if($condition3){
+            echo "Okay! Condition 3 \n";
+            if($condition4){
+                echo "Okay! Condition 4 \n";
+            }else{
+                echo "Else no 4 \n";
+            }
+        } else {
+            echo "Else no 3 \n";
+        }
+    } else{
+        echo "Else no 2 \n";
     }
 } else{
-    echo "Hello False 1 \n";
+    echo " Else No 1 \n";
 }
-?>
-2nd
-<?php
 
-if(true){
-    echo "True 1 \n";
-    if(false){
-        echo "True 2 \n";
-        if (true){
-            echo "True 3 \n";
-        }else {
-            echo "False 3 \n";
-        }
-    } else {
-        echo "Hello False 2 \n";
-    }
-}else{
-    echo "False 1 \n";
+echo "\n";
+
+if($condition1 && $condition2 && $condition3 && $condition4){
+    echo "Okay! Condition 1, 2, 3, 4 \n";
+} elseif($condition1 && $condition2 && $condition3){
+    echo "Okay! Condition 1, 2, 3 \n";
+} elseif($condition1 && $condition2){
+    echo "Okay! Condition 1, 2 \n";
+} elseif($condition1){
+    echo "Okay! Condition 1";
+} else{
+    echo "Else is Okay";
 }
