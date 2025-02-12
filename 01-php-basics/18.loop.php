7968456eeaacaab82049bb0ea0f9beb12a783552
faisalahammed044274 <faisalahammed044274@gmail.com>
2025-02-13 01:37:11 +0600

    for loop

---
+++ 01-php-basics/18.loop.php
@@ -0,0 +1,24 @@
+<?php
+
+// foreach
+// for
+// while
+// do while 
+// goto
+
+// for ($i = 1; $i < 10; $i++) {
+//     // echo $i . "\n";
+//     echo PHP_EOL;
+//      for ($j=0; $j<$i; $j++){
+//     echo "*";
+//     }
+// }
+
+
+for($a =0; $a <10; $a++){
+    echo "\n";
+    for($b = 0; $b < $a; $b++){
+        echo "*";
+        }
+}
+
