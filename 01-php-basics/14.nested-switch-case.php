6b20a8ec961278ca2b10bc675a702d535c66ef71
faisalahammed044274 <faisalahammed044274@gmail.com>
2025-02-13 00:11:12 +0600

    nested switch case

---
+++ 01-php-basics/14.nested-switch-case.php
@@ -0,0 +1,43 @@
+<?php
+
+$n = 11;
+
+$r  = $n % 2;
+
+switch($r){
+    case 0:
+        switch(true){
+            case $n > 0:
+                echo "$n is a Positive Even number";
+                break;
+            case $n < 0:
+                echo "$n is a Negative Even number";
+                break;
+        }
+        break;
+    default:
+    switch($n){
+        case $n > 0:
+            echo "$n is a positive Odd number";
+            break;
+        case $n < 0:
+            echo "$n is a Negative Odd number";
+            break;
+    }
+    
+}
+
+switch(true){
+    case ($r==0 && $n>0):
+        echo "$n is a Positive Even number";
+        break;
+    case ($r==1 && $n<0):
+        echo "$n is a Negative Odd number";
+        break;
+    case ($r==1 && $n>0):
+        echo "$n is a Positive Odd number";
+        break;
+    case ($r==0 && $n<0):
+        echo "$n is a Negative Even number";
+        break;
+}
