a2bee4d84ddb94c829633073b59076fe17f242c7
faisalahammed044274 <faisalahammed044274@gmail.com>
2025-02-13 01:42:24 +0600

    while and do while loop

--- 01-php-basics/18.loop.php
+++ 01-php-basics/18.loop.php
@@ -21,4 +21,20 @@ for($a =0; $a <10; $a++){
         echo "*";
         }
 }
+echo PHP_EOL;
+$x = 0;
 
+while($x < 10){
+$x++;
+echo $x. PHP_EOL;
+}
+
+echo PHP_EOL;
+
+$c = 24;
+
+do{
+    $c++;
+    echo $c . PHP_EOL;
+    } while ($c < 30);
+    ?>
