e45f568a6d73fd9293e152a974d523563f40c760
faisalahammed044274 <faisalahammed044274@gmail.com>
2025-02-13 00:32:42 +0600

    string uses in switch case

--- 01-php-basics/14.nested-switch-case.php
+++ 01-php-basics/14.nested-switch-case.php
@@ -4,9 +4,9 @@ $n = -11;
 
 $r  = $n % 2;
 
-switch($r){
+switch ($r) {
     case 0:
-        switch(true){
+        switch (true) {
             case $n > 0:
                 echo "$n is a Positive Even number";
                 break;
@@ -16,28 +16,47 @@ switch($r){
         }
         break;
     default:
-    switch($n){
-        case $n > 0:
-            echo "$n is a positive Odd number";
-            break;
-        case $n < 0:
-            echo "$n is a Negative Odd number";
-            break;
-    }
-    
+        switch ($n) {
+            case $n > 0:
+                echo "$n is a positive Odd number";
+                break;
+            case $n < 0:
+                echo "$n is a Negative Odd number";
+                break;
+        }
 }
 
-switch(true){
-    case ($r==0 && $n>0):
+echo PHP_EOL;
+
+switch (true) {
+    case ($r == 0 && $n > 0):
         echo "$n is a Positive Even number";
         break;
-    case ($r==-1 && $n<0):
+    case ($r == -1 && $n < 0):
         echo "$n is a Negative Odd number";
         break;
-    case ($r==1 && $n>0):
+    case ($r == 1 && $n > 0):
         echo "$n is a Positive Odd number";
         break;
-    case ($r==0 && $n<0):
+    case ($r == 0 && $n < 0):
         echo "$n is a Negative Even number";
+        break;
+}
+
+echo PHP_EOL;
+
+$string = "8balls";
+switch ($string) {
+    case (string) "9balls":
+        echo "9balls";
+        break;
+    case (string)"8":
+        echo "8";
+        break;
+    case (string)"8balls":
+        echo "8balls";
+        break;
+    case (string)"7balls":
+        echo "7balls";
         break;
 }
