606fa5aa771686d9f41d9b0e3e1eb496be775c4b
faisalahammed044274 <faisalahammed044274@gmail.com>
2025-03-20 16:30:49 +0600

    Loop

--- 01-php-basics/18.loop.php
+++ 01-php-basics/18.loop.php
@@ -1,14 +1,31 @@
 <?php
 
 for ($i = 1; $i <= 10; $i++) {
-    echo $i;
+    // echo $i;
     echo PHP_EOL;
     for ($j = 0; $j < $i; $j++) {
         echo "*";
     }
     echo PHP_EOL; // Add a line break after the stars
 }
+echo PHP_EOL;
+$i = 0;
 
+while($i < 10){
+    $i++;
+    echo $i.PHP_EOL;
+}
 
+$i=0;
+
+do{
+    $i++;
+    echo $i .PHP_EOL;
+} while($i < 10);
+
+$i = 0;
+
+a: $i++;
+echo $i .PHP_EOL;
 
 
