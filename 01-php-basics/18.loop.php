9cf5ebd0ec950638a04180d46bb3065b0fc22fa1
faisalahammed044274 <faisalahammed044274@gmail.com>
2025-03-20 16:07:51 +0600

    loop

--- 01-php-basics/18.loop.php
+++ 01-php-basics/18.loop.php
@@ -1,40 +1,11 @@
 <?php
 
-// foreach
-// for
-// while
-// do while 
-// goto
+for ($i = 1; $i <= 10; $i++) {
+    echo $i . PHP_EOL;
+}
 
-// for ($i = 1; $i < 10; $i++) {
-//     // echo $i . "\n";
-//     echo PHP_EOL;
-//      for ($j=0; $j<$i; $j++){
-//     echo "*";
-//     }
+// for ($i = 10; $i >= 1; $i--) {
+//     echo $i . PHP_EOL;
 // }
 
 
-for($a =0; $a <10; $a++){
-    echo "\n";
-    for($b = 0; $b < $a; $b++){
-        echo "*";
-        }
-}
-echo PHP_EOL;
-$x = 0;
-
-while($x < 10){
-$x++;
-echo $x. PHP_EOL;
-}
-
-echo PHP_EOL;
-
-$c = 24;
-
-do{
-    $c++;
-    echo $c . PHP_EOL;
-    } while ($c < 30);
-?>
