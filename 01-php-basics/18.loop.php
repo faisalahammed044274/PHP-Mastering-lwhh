763d30190f8e173a718a2c4e69c0940cd27fdca8
faisalahammed044274 <faisalahammed044274@gmail.com>
2025-04-08 13:43:12 +0600

    Loops in PHP

--- 01-php-basics/18.loop.php
+++ 01-php-basics/18.loop.php
@@ -1,31 +1,39 @@
 <?php
-
-for ($i = 1; $i <= 10; $i++) {
-    // echo $i;
+echo "======= For Loop ==========";
+//for loop
+for ($i = 1; $i <= 10; $i+=1){
+    echo $i;
     echo PHP_EOL;
-    for ($j = 0; $j < $i; $j++) {
+    for($j=0; $j < $i; $j++){
         echo "*";
     }
-    echo PHP_EOL; // Add a line break after the stars
 }
 echo PHP_EOL;
+echo "======= While Loop ==========";
+//While Loop
+echo PHP_EOL;
 $i = 0;
-
 while($i < 10){
     $i++;
-    echo $i.PHP_EOL;
+    echo $i. PHP_EOL;
 }
-
-$i=0;
-
+echo PHP_EOL;
+echo "======= Do While Loop ==========";
+//Do While Loop
+echo PHP_EOL;
+$i = 5;
 do{
     $i++;
-    echo $i .PHP_EOL;
-} while($i < 10);
+    echo $i . PHP_EOL;
 
+}while($i < 10);
+
+echo PHP_EOL;
+echo "======= Go to ==========";
+echo PHP_EOL;
 $i = 0;
-
-a: $i++;
-echo $i .PHP_EOL;
+a : $i++;
+echo $i . PHP_EOL;
+if($i < 15) goto a;
 
 
