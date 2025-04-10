cb23bd25b3f5f5016b514667322a36c9668cd48f
faisalahammed044274 <faisalahammed044274@gmail.com>
2025-04-10 19:12:14 +0600

    while loop

---
+++ 01-php-basics/23.while-loop.php
@@ -0,0 +1,17 @@
+<?php
+$i = 0;
+while($i <= 10){
+    echo $i;
+    echo PHP_EOL;
+    $i++;
+}
+echo PHP_EOL;
+$j = 0;
+while($j++<5){
+    echo $j;
+    echo PHP_EOL;
+}
+echo "fghgf\n";
+$x = $y = 5;
+$x = $y++;
+echo $x ." : " . $y;
